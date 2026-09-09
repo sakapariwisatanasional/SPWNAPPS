ecf7fb9701b72c64d7f3b3c8dad9a66353c5f009
sakapariwisatanasional <sakapariwisatanasional@gmail.com>
2026-09-09 23:25:54 +0700

    Update DigitalMemberCard.tsx

--- src/components/member/DigitalMemberCard.tsx
+++ src/components/member/DigitalMemberCard.tsx
@@ -99,22 +99,14 @@ export const DigitalMemberCard: React.FC<Props> = ({ member, onEditCard, onPrint
               className="absolute"
               style={{
                 left: `${settings.qrX ?? 78}%`,
-                top: `${settings.qrY ?? 30}%`,
-                zIndex: 20
+                top: `${settings.qrY ?? 30}%`
               }}
             >
               <KtaQrCode
                 member={member}
-                size={Math.max(
-                  36,
-                  Math.round(
-                    Math.min(widthPx, heightPx) *
-                    ((settings.qrSize ?? 22) / 100)
-                  )
-                )}
+                size={Math.max(36, Math.round(Math.min(widthPx, heightPx) * ((settings.qrSize ?? 22) / 100)))}
                 showLabel={false}
                 interactive={false}
-                className="!m-0"
               />
             </div>
           )}
