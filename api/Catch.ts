1bd59165a7c386b0f52566ef9e2719ee48f50f84
sakapariwisatanasional <sakapariwisatanasional@gmail.com>
2026-09-13 10:54:03 +0700

    Add files via upload

---
+++ api/Catch.ts
@@ -0,0 +1,5 @@
+// Catch-all Vercel serverless entrypoint for every /api/* request.
+// The Express application contains the actual /api/... routes.
+import app from './index';
+
+export default app;
