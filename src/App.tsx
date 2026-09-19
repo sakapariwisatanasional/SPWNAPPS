e07b1683e3f5db501ea8bc6248e57f0a07e69c2d
sakapariwisatanasional <sakapariwisatanasional@gmail.com>
2026-09-19 13:43:46 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+
+    
+  
 import React, { useState, useEffect } from 'react';
 import { 
   Member, 
@@ -94,4 +97,4 @@ const ROUTE_TO_TAB: Record<string, string> = {
 // Layout Components
 import { Sidebar } from './components/layout/Sidebar';
 import { Header } from './components/layout/Header';
-export default App;
+
